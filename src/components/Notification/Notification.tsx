448cbff92796447382726a951f20a30a0fbea411
Ihor Shuduria <igorshudurya@gmail.com>
2025-09-21 14:18:37 +0200

    Fix Notification.tsx

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -1,5 +1,5 @@
 import css from "./Notification.module.css";
 
-export default function message() {
+export default function Notification() {
   return <p className={css.message}>No feedback yet</p>;
 }
